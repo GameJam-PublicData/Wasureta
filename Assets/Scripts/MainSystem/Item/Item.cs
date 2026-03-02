4d8c42787d3be088a9d38d18d4e50fedffbb417e
Axzin <Pchan0961@gmail.com>
2026-03-02 13:09:51 +0900

    Item関係を定義

---
+++ Assets/Scripts/MainSystem/Item/Item.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace MainSystem.Item
+{
+public interface IItem 
+{
+}
+public class Item : MonoBehaviour,IItem
+{
+}
+}
+  
+  
---
+++ Assets/Scripts/MainSystem/Item/ItemManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainSystem.Item
+{
+public interface IItemManager
+{
+    void AddItem(IItem item);
+    (List<IItem> lostItems, List<IItem> otherItems) GetItems();
+}
+
+public class ItemManager : IItemManager
+{
+    ItemSO _itemSO;
+
+    public ItemManager(ItemSO itemSO)
+    {
+        _itemSO = itemSO;
+    }
+
+    List<IItem> _itemList = new();
+
+    public void AddItem(IItem item)
+    {
+        if (_itemList.Contains(item))
+        {
+            Debug.LogError("同じアイテムが既に存在しています。");
+            return;
+        }
+
+        _itemList.Add(item);
+    }
+
+    public (List<IItem> lostItems, List<IItem> otherItems) GetItems()
+    {
+        List<IItem> lostItems = new();
+        List<IItem> otherItems = new();
+
+        foreach (var item in _itemList)
+        {
+            if (_itemSO.LostIItemList.Contains(item))
+            {
+                lostItems.Add(item);
+            }
+            else
+            {
+                otherItems.Add(item);
+            }
+        }
+
+        return (lostItems, otherItems);
+    }
+}
+}
