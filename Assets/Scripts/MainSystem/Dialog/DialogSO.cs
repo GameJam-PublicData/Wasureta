f490d9e970b6c46e335f5b48380bc9f608ee9e46
ta-ku44 <213548904+ta-ku44@users.noreply.github.com>
2026-03-03 21:55:31 +0900

    feat: DialogSOにRushIntroContentを追加し、Content変数名を変更

--- Assets/Scripts/MainSystem/Dialog/DialogSO.cs
+++ Assets/Scripts/MainSystem/Dialog/DialogSO.cs
@@ -5,7 +5,8 @@ namespace MainSystem.Dialog
 [CreateAssetMenu(fileName = "DialogSO", menuName = "ScriptableObject/DialogSO")]
 public class DialogSO : ScriptableObject
 {
-    public string Content = "Dialog Content";
+    public string DialogContent = "Dialog Content";
+    public string RushIntroContent = "Forget Content";
     public float CharInterval = 0.1f;
     public float LineEndDelay = 1f;
 }
