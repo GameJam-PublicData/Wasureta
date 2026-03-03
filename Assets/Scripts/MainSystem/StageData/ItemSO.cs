9ec2febf51b1954a9c2ed4af86932bacfd20b245
Axzin <Pchan0961@gmail.com>
2026-03-03 14:23:53 +0900

    微修正

--- Assets/Scripts/MainSystem/StageData/ItemSO.cs
+++ Assets/Scripts/MainSystem/StageData/ItemSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using StageSystem.Item;
 using UnityEngine;
@@ -7,8 +8,36 @@ namespace MainSystem.StageData
 [CreateAssetMenu(menuName = "ScriptableObject/Item")]
 public class ItemSO : ScriptableObject
 {
-    [SerializeField] List<Item> itemList = new();
-    
+    [SerializeField] GameObject itemRootPrefab;
+    [SerializeField,HideInInspector] List<Item> itemList = new();
+
+    void OnValidate()
+    {
+        if (itemRootPrefab == null)
+        {
+            Debug.LogError("アイテムのルートプレハブが設定されていません。");
+            return;
+        }
+
+        for (int i = 0; i < itemRootPrefab.transform.childCount; i++)
+        {
+            var child = itemRootPrefab.transform.GetChild(i).gameObject;
+            var itemComponent = child.GetComponent<Item>();
+            if (itemComponent == null)
+            {
+                Debug.LogError($"子オブジェクト '{child.name}' に Item コンポーネントがアタッチされていません。");
+                continue;
+            }
+
+            if (!itemList.Contains(itemComponent))
+            {
+                itemList.Add(itemComponent);
+            }
+        }
+
+    }
+
+
     public List<Item> LostItemList => itemList;
     public List<IItem> LostIItemList => itemList.ConvertAll(item => item as IItem);
 }
