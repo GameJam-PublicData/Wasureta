0d3123567bbc731d043ae985247003ea7c02173e
LRGyutoi <yuuto_24s1101205@nnn.ed.jp>
2026-03-04 12:27:48 +0900

    Doorが開くようになった

--- Assets/Scripts/StageSystem/Interact/SwingingDoorController.cs
+++ Assets/Scripts/StageSystem/Interact/SwingingDoorController.cs
@@ -5,12 +5,26 @@ namespace StageSystem.Interact
 {
 public class SwingingDoorController : MonoBehaviour, IInteractive
 {
-    [SerializeField]float openTime = 5f;
-    [SerializeField]float angle = 90f;
-    
+    [SerializeField] float openTime = 5f;
+    [SerializeField] float angle = 90f;
+
+    bool _isOpen = false;
+    bool _isAnimating = false;
+
     public void Interact()
     {
-        transform.DORotate(new Vector3(0, angle, 0), openTime , RotateMode.LocalAxisAdd);
+        // アニメーション中は無視
+        if (_isAnimating) return;
+
+        _isAnimating = true;
+        float targetAngle = _isOpen ? -angle : angle;
+
+        transform.DORotate(new Vector3(0, targetAngle, 0), openTime, RotateMode.LocalAxisAdd)
+            .OnComplete(() =>
+            {
+                _isOpen = !_isOpen;
+                _isAnimating = false;
+            });
     }
 }
 }
